99eaf2e78786c3b0b529bc1a05f61ee5947f725f
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2025-08-09 15:31:40 +0800

    Add files via upload
    ver.105

---
+++ CreateAccount_Form.Designer.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Jrr_Transport_Management_System
+{
+    partial class CreateAccount_Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateAccount_Form));
+            C_username = new Label();
+            C_password = new Label();
+            label3 = new Label();
+            C_user_name = new TextBox();
+            C_pass_word = new TextBox();
+            C_acc_btn = new Button();
+            L_acc_link = new LinkLabel();
+            SuspendLayout();
+            // 
+            // C_username
+            // 
+            resources.ApplyResources(C_username, "C_username");
+            C_username.Name = "C_username";
+            // 
+            // C_password
+            // 
+            resources.ApplyResources(C_password, "C_password");
+            C_password.Name = "C_password";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+          
+            // 
+            // C_user_name
+            // 
+            resources.ApplyResources(C_user_name, "C_user_name");
+            C_user_name.Name = "C_user_name";
+            // 
+            // C_pass_word
+            // 
+            resources.ApplyResources(C_pass_word, "C_pass_word");
+            C_pass_word.Name = "C_pass_word";
+            // 
+            // C_acc_btn
+            // 
+            C_acc_btn.BackColor = Color.DeepSkyBlue;
+            resources.ApplyResources(C_acc_btn, "C_acc_btn");
+            C_acc_btn.ForeColor = SystemColors.ButtonFace;
+            C_acc_btn.Name = "C_acc_btn";
+            C_acc_btn.UseVisualStyleBackColor = false;
+            // 
+            // L_acc_link
+            // 
+            resources.ApplyResources(L_acc_link, "L_acc_link");
+            L_acc_link.LinkColor = Color.Red;
+            L_acc_link.Name = "L_acc_link";
+            L_acc_link.TabStop = true;
+            // 
+            // CreateAccount_Form
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            Controls.Add(L_acc_link);
+            Controls.Add(C_acc_btn);
+            Controls.Add(C_pass_word);
+            Controls.Add(C_user_name);
+            Controls.Add(label3);
+            Controls.Add(C_password);
+            Controls.Add(C_username);
+            DoubleBuffered = true;
+            ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "CreateAccount_Form";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label C_username;
+        private Label C_password;
+        private Label label3;
+        private TextBox C_user_name;
+        private TextBox C_pass_word;
+        private Button C_acc_btn;
+        private LinkLabel L_acc_link;
+    }
+}
---
+++ dashboard.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Jrr_Transport_Management_System
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(427, 246);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 31);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.d879b042_9903_4ae7_b02b_cd5c3f2a037a_removebg_preview;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(1352, 531);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Dashboard";
+            Text = "Dashboard";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
