f52c0d12bf7ee925eb5f530b898d876ec0ad251e
ddebi18 <dave.rentoria18@gmail.com>
2025-08-10 23:32:31 +0800

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Jrr_Transport_Management_System
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            button1 = new Button();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 52, 61);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 74);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(0, 52, 61);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(12, 105);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(183, 333);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ActiveBorder;
+            panel3.Location = new Point(213, 105);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(251, 333);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ActiveBorder;
+            panel4.Location = new Point(484, 105);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(304, 333);
+            panel4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(86, 18);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 37);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Button button1;
+    }
+}
