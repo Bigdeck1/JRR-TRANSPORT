ae263c6d869c0244623741edecee19adfd43a77d
ddebi18 <dave.rentoria18@gmail.com>
2025-08-12 22:03:27 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,15 +1,14 @@
-﻿
-namespace Jrr_Transport_Management_System
+﻿namespace Business_Management_System
 {
-    partial class login_Form
+    partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,95 +23,243 @@ namespace Jrr_Transport_Management_System
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login_Form));
-            login_Btn = new Button();
-            pass_word = new TextBox();
-            user_name = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            acc_Create = new Label();
-            label3 = new Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // login_Btn
+            // panel1
             // 
-            login_Btn.BackColor = Color.DeepSkyBlue;
-            resources.ApplyResources(login_Btn, "login_Btn");
-            login_Btn.ForeColor = SystemColors.ButtonFace;
-            login_Btn.Name = "login_Btn";
-            login_Btn.UseVisualStyleBackColor = false;
+            panel1.BackColor = Color.White;
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 74);
+            panel1.TabIndex = 0;
             // 
-            // pass_word
+            // panel2
             // 
-            resources.ApplyResources(pass_word, "pass_word");
-            pass_word.Name = "pass_word";
+            panel2.BackColor = Color.FromArgb(0, 52, 61);
+            panel2.Controls.Add(pictureBox5);
+            panel2.Controls.Add(pictureBox4);
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(12, 105);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(183, 333);
+            panel2.TabIndex = 1;
             // 
-            // user_name
+            // pictureBox5
             // 
-            resources.ApplyResources(user_name, "user_name");
-            user_name.Name = "user_name";
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(3, 194);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(84, 71);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
             // 
-            // label1
+            // pictureBox4
             // 
-            resources.ApplyResources(label1, "label1");
-            label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Name = "label1";
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(3, 129);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(84, 71);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
             // 
-            // label2
+            // pictureBox3
             // 
-            resources.ApplyResources(label2, "label2");
-            label2.ForeColor = SystemColors.ActiveCaptionText;
-            label2.Name = "label2";
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(3, 259);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(84, 71);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
             // 
-            // acc_Create
+            // pictureBox2
             // 
-            resources.ApplyResources(acc_Create, "acc_Create");
-            acc_Create.FlatStyle = FlatStyle.Flat;
-            acc_Create.ForeColor = Color.IndianRed;
-            acc_Create.Name = "acc_Create";
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 65);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(84, 71);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
             // 
-            // label3
+            // pictureBox1
             // 
-            resources.ApplyResources(label3, "label3");
-            label3.ForeColor = SystemColors.ActiveCaptionText;
-            label3.Name = "label3";
-            
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(84, 71);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
-            // login_Form
+            // button5
             // 
-            resources.ApplyResources(this, "$this");
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(80, 271);
+            button5.Name = "button5";
+            button5.Size = new Size(91, 47);
+            button5.TabIndex = 4;
+            button5.Text = "SETTING";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(80, 206);
+            button4.Name = "button4";
+            button4.Size = new Size(91, 47);
+            button4.TabIndex = 3;
+            button4.Text = "REPORTS";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(80, 142);
+            button3.Name = "button3";
+            button3.Size = new Size(91, 47);
+            button3.TabIndex = 2;
+            button3.Text = "PAYROLL";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(80, 76);
+            button2.Name = "button2";
+            button2.Size = new Size(91, 47);
+            button2.TabIndex = 1;
+            button2.Text = "USER";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(80, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 47);
+            button1.TabIndex = 0;
+            button1.Text = "HOME";
+            button1.TextImageRelation = TextImageRelation.TextAboveImage;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Silver;
+            panel3.Location = new Point(213, 105);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(251, 333);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Silver;
+            panel4.Location = new Point(484, 105);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(304, 333);
+            panel4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(224, 224, 224);
-            Controls.Add(label3);
-            Controls.Add(acc_Create);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(user_name);
-            Controls.Add(pass_word);
-            Controls.Add(login_Btn);
-            DoubleBuffered = true;
-            ForeColor = SystemColors.ActiveBorder;
+            BackColor = Color.Lavender;
+            ClientSize = new Size(800, 447);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            Name = "login_Form";
-            WindowState = FormWindowState.Maximized;
-           
+            Name = "Form1";
+            Text = "Form2";
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button login_Btn;
-        private TextBox pass_word;
-        private TextBox user_name;
-        private Label label1;
-        private Label label2;
-        private Label acc_Create;
-        private Label label3;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Button button1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,115 +1,20 @@
-using System;
-using System.Data.SQLite;
-using System.IO;
-using System.Security.Cryptography;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Jrr_Transport_Management_System
+namespace Business_Management_System
 {
-    public partial class login_Form : Form
+    public partial class Form1 : Form
     {
-        private string dbFile = "jrr_transport_data.db";
-
-        public login_Form()
+        public Form1()
         {
             InitializeComponent();
-            EnsureDatabase();
-            login_Btn.Click += login_Btn_Click;
-            acc_Create.Click += acc_Create_Click;
-        }
-
-        private void EnsureDatabase()
-        {
-            if (!File.Exists(dbFile))
-            {
-                SQLiteConnection.CreateFile(dbFile);
-            }
-
-            using (var conn = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
-            {
-                conn.Open();
-
-                string createUsersTable = @"
-                    CREATE TABLE IF NOT EXISTS Users (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Username TEXT NOT NULL UNIQUE,
-                        PasswordHash TEXT NOT NULL
-                    );
-                ";
-
-                using (var cmd = new SQLiteCommand(createUsersTable, conn))
-                {
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        private void login_Btn_Click(object? sender, EventArgs e)
-        {
-            string username = user_name.Text?.Trim() ?? string.Empty;
-            string password = pass_word.Text?.Trim() ?? string.Empty;
-
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-            {
-                MessageBox.Show("Please enter username and password.");
-                return;
-            }
-
-            if (VerifyLogin(username, password))
-            {
-                MessageBox.Show("Login successful!");
-                Dashboard dash = new Dashboard();
-                this.Hide();
-                dash.FormClosed += (s, args) => this.Show();
-                dash.Show();
-            }
-            else
-            {
-                MessageBox.Show("Invalid username or password.");
-            }
-        }
-
-        private bool VerifyLogin(string username, string password)
-        {
-            using (var conn = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
-            {
-                conn.Open();
-                string query = "SELECT PasswordHash FROM Users WHERE Username=@user LIMIT 1";
-                using (var cmd = new SQLiteCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@user", username);
-                    object? result = cmd.ExecuteScalar();
-
-                    if (result != null)
-                    {
-                        string storedHash = result.ToString() ?? string.Empty;
-                        string inputHash = HashPassword(password);
-                        return storedHash == inputHash;
-                    }
-                }
-            }
-            return false;
-        }
-
-        private string HashPassword(string password)
-        {
-            using (var sha256 = SHA256.Create())
-            {
-                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                StringBuilder sb = new StringBuilder();
-                foreach (byte b in bytes)
-                    sb.Append(b.ToString("x2")); // Hex format
-                return sb.ToString();
-            }
-        }
-
-        private void acc_Create_Click(object? sender, EventArgs e)
-        {
-            CreateAccount_Form createForm = new CreateAccount_Form();
-            this.Hide();
-            createForm.FormClosed += (s, args) => this.Show();
-            createForm.Show();
         }
     }
 }
